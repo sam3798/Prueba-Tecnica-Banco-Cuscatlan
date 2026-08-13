160096a07fd3c20ba1f430459f82990176dc0cf4
Samuel Gonzalez <samuel_gonzalez3798@outlook.com>
2026-08-13 09:34:29 -0600

    Commit inicial: Configuración primitiva del proyecto y primera compilación

---
+++ src/test/java/sv/bancocuscatlan/coworking/CoworkingReservasApplicationTests.java
@@ -0,0 +1,12 @@
+package sv.bancocuscatlan.coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingReservasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
